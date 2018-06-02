f7b61394ee73222f7785c0a365d9817543bc3ba4
ljy0130 <lhkshjy@gmail.com>
2018-06-02 07:03:31 +0000

    push container2

--- node/server.js
+++ node/server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('180602 TEST Hello world!\n');
+  res.send('180602 22222222 LJY Hello world!\n');
 });
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
